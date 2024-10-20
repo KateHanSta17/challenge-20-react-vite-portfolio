08fc2c0d7aad7bd069068cde524680b03bc6b6ed
Hank Star <kate.stapley@hotmail.com>
2024-10-20 23:26:52 +1100

    code cloned from old repo into new for pages

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,13 @@
+// src/pages/Resume.jsx
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <main className="container">
+      <h2 className="portfolio-title">Resume</h2>
+      <p>This is where your resume will go.</p>
+    </main>
+  );
+};
+
+export default Resume;
